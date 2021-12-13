7c217e80c3dbe7f38b9364abcceb95073b8b89f5
„justyna” <justyna.lipska@allegro.pl>
2021-12-13 12:34:41 +0000

    add bigquery export

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -8,6 +8,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.bigquery.storage.v1.*;
+import com.google.protobuf.Descriptors;
+import org.json.JSONArray;
+import org.json.JSONObject;
+ 
+import java.io.IOException;
+import java.time.Instant;
+
 
 @SpringBootApplication
 @RestController
@@ -39,6 +49,60 @@ public class Application {
     public Arena arena;
   }
 
+  static class WriteCommittedStream {
+
+    final JsonStreamWriter jsonStreamWriter;
+
+    public WriteCommittedStream(String projectId, String datasetName, String tableName) throws IOException, Descriptors.DescriptorValidationException, InterruptedException {
+
+      try (BigQueryWriteClient client = BigQueryWriteClient.create()) {
+
+        WriteStream stream = WriteStream.newBuilder().setType(WriteStream.Type.COMMITTED).build();
+        TableName parentTable = TableName.of(projectId, datasetName, tableName);
+        CreateWriteStreamRequest createWriteStreamRequest =
+                CreateWriteStreamRequest.newBuilder()
+                        .setParent(parentTable.toString())
+                        .setWriteStream(stream)
+                        .build();
+
+        WriteStream writeStream = client.createWriteStream(createWriteStreamRequest);
+
+        jsonStreamWriter = JsonStreamWriter.newBuilder(writeStream.getName(), writeStream.getTableSchema()).build();
+      }
+    }
+
+    public ApiFuture<AppendRowsResponse> send(Arena arena) {
+      Instant now = Instant.now();
+      JSONArray jsonArray = new JSONArray();
+
+      arena.state.forEach((url, playerState) -> {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("x", playerState.x);
+        jsonObject.put("y", playerState.y);
+        jsonObject.put("direction", playerState.direction);
+        jsonObject.put("wasHit", playerState.wasHit);
+        jsonObject.put("score", playerState.score);
+        jsonObject.put("player", url);
+        jsonObject.put("timestamp", now.getEpochSecond() * 1000 * 1000);
+        jsonArray.put(jsonObject);
+      });
+
+      return jsonStreamWriter.append(jsonArray);
+    }
+
+  }
+
+  final String projectId = ServiceOptions.getDefaultProjectId();
+  final String datasetName = "snowball";
+  final String tableName = "events";
+
+  final WriteCommittedStream writeCommittedStream;
+
+  public Application() throws Descriptors.DescriptorValidationException, IOException, InterruptedException {
+    writeCommittedStream = new WriteCommittedStream(projectId, datasetName, tableName);
+  }
+
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
@@ -58,8 +122,9 @@ public class Application {
     System.out.println(arenaUpdate);
     String[] commands = new String[]{"F", "R", "L", "T"};
     int i = new Random().nextInt(4);
-    //return commands[i];
-    return "T";
+    writeCommittedStream.send(arenaUpdate.arena);
+    return commands[i];
+
 
    /* if (arenaUpdate.arena.state.isEmpty()) {
         return "F";
@@ -80,5 +145,7 @@ public class Application {
     }
     if (playerState.y + 1 == )
    }*/
+}
+
 }
 
